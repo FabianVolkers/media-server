eac6d6704f8237d8d424a32dbd434667b5689297
Fabian Volkers <fabianvolkers@gmail.com>
2019-05-18 19:50:07 +0200

    spannend

--- feature/when-is-zac-coming-home/js/zac_flights.php
+++ feature/when-is-zac-coming-home/js/zac_flights.php
@@ -1,9 +1,13 @@
 <html>
     <body>
 <?php
-        echo POST["new-city"];
+        $myFile = fopen("data.json", "a") or die ("Unable to open file");
+        $city = POST["new-city"];
+        $date = strToTime(POST["new-date"]);
         echo POST["new-date"];
         echo POST["new-flights"];
+        $destinationObject = "{'city' : " . $city . ",'date' : ". $date . "},";
+        fwrite($myFile, $destinationObject);
       ?>
 </body>
 </html>
