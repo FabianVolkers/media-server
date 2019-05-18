8c8385a44193055bd6a488419e58fb69e14e1bde
Fabian Volkers <fabianvolkers@gmail.com>
2019-05-18 17:50:08 +0200

    i dont know what I'm doing

--- feature/when-is-zac-coming-home/js/zac_flights.php
+++ feature/when-is-zac-coming-home/js/zac_flights.php
@@ -1,6 +1,10 @@
+<html>
+    <body>
 <?php
 echo "Hello World!";
 $dataFile = fopen("data.json", "r") or die ("Unable to open file");
 echo fread(dataFile, filesize("data.json"));
 fclose($dataFile);
 ?>
+</body>
+</html>
