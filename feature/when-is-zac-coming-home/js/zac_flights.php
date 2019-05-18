e643d5a049f01013c1ac0dd1a3bfe303337d2a65
Fabian Volkers <fabianvolkers@gmail.com>
2019-05-18 20:07:52 +0200

    maybe so

--- feature/when-is-zac-coming-home/js/zac_flights.php
+++ feature/when-is-zac-coming-home/js/zac_flights.php
@@ -3,12 +3,15 @@
 <body>
     <?php
         $myFile = fopen("data.json", "a") or die ("Unable to open file");
+        $file = file_get_contents("data.json");
+        $data = json_decode($file);
+        $data[count($data)]["city"] = $_POST["new-city"];
         $city = $_POST["new-city"];
         $date = strToTime(POST["new-date"]);
         echo $_POST["new-date"];
         echo $_POST["new-flights"];
         $destinationObject = "{'city' : " . $city . ",'date' : ". $date . "},";
-        fwrite($myFile, $destinationObject);
+        file_put_contents('data.json',json_encode($data));
         fclose($myFile);
       ?>
 </body>
